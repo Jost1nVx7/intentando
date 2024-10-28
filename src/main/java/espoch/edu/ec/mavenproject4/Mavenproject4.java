d727c50dd92a786a34c00c878a102c44eef27376
Jost1nVx7 <jostinjd2002@gmail.com>
2024-10-28 17:25:00 -0500

    BASTA

---
+++ src/main/java/espoch/edu/ec/mavenproject4/Mavenproject4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package espoch.edu.ec.mavenproject4;
+
+/**
+ *
+ * @author Global Technology Ec
+ */
+public class Mavenproject4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
